99573e7a919a5ba918f3f2ad9b67fdf71f2105e4
abcdana <abcdanakim@gmail.com>
2021-11-27 18:36:28 +0900

    Initial commit

---
+++ src/main/java/com/test/chatting/Client2.java
@@ -0,0 +1,9 @@
+package com.test.chatting;
+
+
+public class Client2 {
+    public static void main(String[] args) throws InterruptedException {
+        EasyClient easyClient= new EasyClient("localhost",8090);
+        easyClient.start();
+    }
+}
